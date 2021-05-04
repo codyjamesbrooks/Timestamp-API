eda49ca5118bfa30c614d45778afdbcbe28cdf23
codyjamesbrooks <71521423+codyjamesbrooks@users.noreply.github.com>
2021-05-04 13:54:18 -0600

    forking changes

--- server.js
+++ server.js
@@ -2,31 +2,49 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
+var dateFormat = require("dateformat");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+let date;
+// UNIX timestamp JSON response
+app.get("/api/:date", (req, res) => {
+  if (isNaN(req.params.date)) {
+    date = new Date(Date.parse(req.params.date));
+  } else {
+    date = new Date(parseInt(req.params.date));
+  }
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  !!date.getTime()
+    ? res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString(),
+      })
+    : res.json({ error: "Invalid Date" });
 });
 
-
+app.get("/api", (req, res) => {
+  date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
